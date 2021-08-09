c70b36d6314345d71b31cd359840d26b9bdc70c7
cllcnkrt <cllcnkrt@outlook.com>
2021-08-09 11:27:43 +0300

    react router added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
+    <Router>
     <div className="App">
-   <h1>Amazon clone</h1>
+      <Switch>
+        <Route path="/checkout">
+      <h1>Checkout</h1>
+        </Route>
+        <Route path="/login">
+          <h1>Login</h1>
+        </Route>
+        <Route path="/">
+        <h1>Home</h1>
+        </Route>
+      </Switch>
+   
     </div>
+    </Router>
   );
 }
 
