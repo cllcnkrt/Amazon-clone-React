c58f44a7c6102010041fb6e9f5caf78ba3fdd2a0
cllcnkrt <cllcnkrt@outlook.com>
2021-08-10 12:00:51 +0300

    checkout product component

--- src/components/checkout/Checkout.js
+++ src/components/checkout/Checkout.js
@@ -1,18 +1,46 @@
-import React from 'react'
+import React from "react";
 import { useStateValue } from "../../StateProvider";
+import './Checkout.css'
+import CheckoutProduct from "./CheckoutProduct";
 function Checkout() {
+  const [{ basket, user }, dispatch] = useStateValue();
 
-    const [{ basket }, dispatch] = useStateValue();
-    return (
-        <div className="checkout">
-            <img
-          className="checkout-ad"
+  return (
+    <div className="checkout">
+      <div className="checkout__left">
+        <img
+          className="checkout__ad"
           src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
           alt=""
         />
+        {basket?.length === 0 ? (
+            <div>
+            <h2>Your Shopping Basket is empty</h2>
+            <p>
+                You have no items in your basket
+            </p>
+            </div>
 
-        </div>
-    )
+        ):(
+            <div>
+                <h2 className="checkout__title">
+                    Your Shopping Basket
+                </h2>
+
+            {basket.map(item =>(
+               <CheckoutProduct
+               id={item.id}
+               title={item.title}
+               image={item.image}
+               price={item.price}
+               rating={item.rating}
+             />
+            ))}
+            </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default Checkout
+export default Checkout;
